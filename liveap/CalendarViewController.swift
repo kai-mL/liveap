b45285bb4e99a37bf3e5d18d6a49a28f9e473f63
kai-mL <kakerusuzuki09tyoby@gmail.com>
2023-06-13 16:12:40 +0900

    1.0.2

--- liveap/CalendarViewController.swift
+++ liveap/CalendarViewController.swift
@@ -8,34 +8,38 @@
 import UIKit
 import FSCalendar
 import RealmSwift
-import SwiftUI
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController,FSCalendarDataSource,FSCalendarDelegate {
     
     let realm = try! Realm()
     @IBOutlet weak var calendar: FSCalendar!
-    @State var selectedDate = Date()
-    @IBOutlet var eventTitleLabel: UILabel!
-  //  let RealmItems = realm.objects(RealmData.self)
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var eventLabel: UILabel!
+    let df = DateFormatter()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        CalendarView(selectedDate: selectedDate)
-            .frame(height: 400)
-        Text(selectedDate,style: .date)
-            .font(.title)
-            .padding()
+       
+        calendar.dataSource = self
+        calendar.delegate = self
+        view.addSubview(calendar)
+        view.addSubview(dateLabel)
         
- //       if selectedDate == RealmItems.eventDate {
-//           eventTitleLabel.text = eventTitle
-//       }else if selectedDate == RealmItems.entryResultDate {
-//           eventTitleLabel.text = eventTitle + "当落日"
-//       }else if selectedDate == RealmItems.moneyDate {
-//          eventTitleLabel.text = eventTile + "入金日"
-//     }
+        if df == eventDate {
+            eventLabel.text = String(eventTitle)
+        }else if df == eventResultDate {
+            eventLabel.text = String(eventTItle + "当落日")
+        }else if df == moneyDate {
+            eventLabel.text = String(eventTitle + "入金日")
+        }
         
     }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+           df.dateFormat = "yyyy-MM-dd"
+           dateLabel.text = df.string(from: date)
+       }
     
 }
